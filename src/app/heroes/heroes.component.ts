1619a7bd4d68f4f39fb21aa176b2f3c94064cfa0
SANDRO SIMON <sandrosimonore@gmail.com>
2023-08-30 10:34:56 -0500

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+interface Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+

e33cf0ca7c41cb6520b35ffbce000b4a0fbc23e6
SANDRO SIMON <sandrosimonore@gmail.com>
2023-08-30 12:19:20 -0500

    display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from '../interfaces/hero';
 
-interface Hero {
-  id: number;
-  name: string;
-}
+import { HEROES } from '../data/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-}
+  heroes = HEROES;
+  selectedHero?: Hero;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}

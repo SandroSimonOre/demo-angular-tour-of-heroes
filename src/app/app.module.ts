f756f77763593e3b8acc0863d03f9406c52a5966
SANDRO SIMON <sandrosimonore@gmail.com>
2023-09-02 13:10:19 -0500

    add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroeDetailComponent } from './heroe-detail/heroe-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroeDetailComponent],
+  declarations: [AppComponent, HeroesComponent, HeroeDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
